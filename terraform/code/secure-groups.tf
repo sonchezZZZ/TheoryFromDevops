049100c086dfc660cd1165be31440927afc8a443
sonchezZZZ <79608549+sonchezZZZ@users.noreply.github.com>
2022-02-09 22:25:18 +0200

    Update secure-groups.tf

--- terraform/code/secure-groups.tf
+++ terraform/code/secure-groups.tf
@@ -31,3 +31,32 @@ resource "aws_security_group" "my_web_server" { // Creating new security group
     Name = "security-group"
   }
 }
+
+
+// --------------------------------   or if several ports -----------------------
+  
+resource "aws_security_group" "my_web_server" { // Creating new security group
+
+  dynamic "ingress" {
+
+    for_each = ["80","443"]
+    content {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      }
+    }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security-group"
+  }
+}
